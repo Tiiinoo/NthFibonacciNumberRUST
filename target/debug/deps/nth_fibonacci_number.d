3da6d79d82e3a663595971cf48decf4c77c83ee4
Martin Azpiroz <martinazpiroz@MBP-de-Martin.fibertel.com.ar>
2021-06-13 20:24:36 -0300

    Release version updated

---
+++ target/debug/deps/nth_fibonacci_number.d
@@ -0,0 +1,5 @@
+/Users/martinazpiroz/Desktop/RustProjects/nth_fibonacci_number/target/debug/deps/nth_fibonacci_number: src/main.rs
+
+/Users/martinazpiroz/Desktop/RustProjects/nth_fibonacci_number/target/debug/deps/nth_fibonacci_number.d: src/main.rs
+
+src/main.rs:
---
+++ target/release/nth_fibonacci_number.d
@@ -0,0 +1,1 @@
+/Users/martinazpiroz/Desktop/RustProjects/nth_fibonacci_number/target/release/nth_fibonacci_number: /Users/martinazpiroz/Desktop/RustProjects/nth_fibonacci_number/src/main.rs
